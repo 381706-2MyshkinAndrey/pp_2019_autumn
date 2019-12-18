b2cd2e586e55697c2ec347c21915be80e07545ae
381706-2MyshkinAndrey <43260478MyshkinAndrey@users.noreply.github.com>
2019-12-18 15:36:52 +0300

    add new 2.2

--- modules/task_3/myshkin_a_shell_sort/shell_sort.cpp
+++ modules/task_3/myshkin_a_shell_sort/shell_sort.cpp
@@ -9,6 +9,87 @@
 #include <utility>
 #include "../../../modules/task_3/myshkin_a_shell_sort/shell_sort.h"
 
+int* getRandomArrayInt(int sizeA) {
+  if (sizeA <= 0)
+    throw std::runtime_error("Error Array size");
+  std::mt19937 gen;
+  gen.seed(static_cast<unsigned int>(time(0)));
+  int *localBuf = reinterpret_cast<int*>(malloc(sizeof(int) * sizeA));
+  for (int i = 0; i < sizeA; i++) {
+    localBuf[i] = gen() % 100;
+  }
+  return localBuf;
+}
+
+int* ShellSortSenq(int* buffer, int length) {
+  int i, j, temp;
+  // int length = buffer.size();
+  int step = length / 2;
+
+  while (step > 0) {
+    for (i = 0; i < (length - step); i++) {
+      j = i;
+      while (j >= 0 && buffer[j] > buffer[j + step]) {
+        temp = buffer[j];
+        buffer[j] = buffer[j + step];
+        buffer[j + step] = temp;
+        j--;
+      }
+    }
+    step = step / 2;
+  }
+  return buffer;
+}
+
+void ownMergeBound(int *buffer, int left, int right, int middle)
+{
+  if (left >= right || middle < left || middle > right) return;
+  if (right == left + 1 && buffer[left] > buffer[right]) {
+    int value = buffer[right];
+    buffer[right] = buffer[left];
+    buffer[left] = value;
+    return;
+  }
+  int *tmp = (int*)malloc(((right - left) + 1) * sizeof(int));
+  if (tmp == nullptr) return;
+  for (int i = 0; i < ((right - left) + 1); i++) tmp[i] = buffer[left + i];
+
+  for (size_t i = left, j = 0, k = (middle - left + 1); i <= right; ++i) {
+    if (j > middle - left) {
+      buffer[i] = tmp[k++];
+    }
+    else if (k > right - left) {
+      buffer[i] = tmp[j++];
+    }
+    else {
+      buffer[i] = (tmp[j] < tmp[k]) ? tmp[j++] : tmp[k++];
+    }
+  }
+  if (tmp) { free(tmp); tmp = nullptr; }
+
+  return;
+}
+
+void ownMergeSort(int *buffer, int left, int right)
+{
+  if (left >= right) return;
+
+  int middle = left + (right - left) / 2;
+
+  ownMergeSort(buffer, left, middle);
+  ownMergeSort(buffer, (middle + 1), right);
+  ownMergeBound(buffer, left, right, middle);
+  return;
+}
+
+int mergeSort(int *buffer, int size)
+{
+  if ((buffer == nullptr) || (size < 2)) return -1;
+  int left = 0, right = size - 1;
+
+  ownMergeSort(buffer, left, right);
+  return 0;
+}
 
 std::vector<int> getRandomVector(int length) {
   if (length <= 0)
